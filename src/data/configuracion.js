4b2ead421bf274e77619fde7a6a2bbae79dc7ef4
santicampca <accsanti066@gmail.com>
2026-07-30 11:32:23 -0400

    Create configuracion.js

---
+++ src/data/configuracion.js
@@ -0,0 +1,15 @@
+export const configuracion = {
+  moneda: "$",
+
+  pedidoMinimo: 0,
+
+  ciudadPrincipal: "Caracas",
+
+  envioGratis: true,
+
+  costoEnvio: 0,
+
+  permitirPersonalizacion: true,
+
+  permitirMayoristas: true
+};
