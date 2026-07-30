974b6efc4f4e4a5509923084336c7abddaac75a5
santicampca <accsanti066@gmail.com>
2026-07-30 12:09:18 -0400

    Create categorias.js

---
+++ src/data/categorias.js
@@ -0,0 +1,23 @@
+export const categorias = [
+
+  {
+    id: "alinos",
+    nombre: "Aliños"
+  },
+
+  {
+    id: "ensaladas",
+    nombre: "Ensaladas"
+  },
+
+  {
+    id: "combos",
+    nombre: "Combos"
+  },
+
+  {
+    id: "hierbas",
+    nombre: "Hierbas Frescas"
+  }
+
+];
