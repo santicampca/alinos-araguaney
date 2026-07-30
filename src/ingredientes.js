4b41ca08c1f6f271a24a8a593f17a79966cc45d0
santicampca <accsanti066@gmail.com>
2026-07-30 12:01:49 -0400

    Create ingredientes.js

---
+++ src/ingredientes.js
@@ -0,0 +1,51 @@
+export const ingredientes = [
+
+  {
+    id: "cebolla",
+    nombre: "Cebolla",
+    precioKilo: 1.80,
+    disponible: true,
+    color: "#E8D9A8"
+  },
+
+  {
+    id: "cebollin",
+    nombre: "Cebollín",
+    precioKilo: 2.50,
+    disponible: true,
+    color: "#3FA34D"
+  },
+
+  {
+    id: "cilantro",
+    nombre: "Cilantro",
+    precioKilo: 3.20,
+    disponible: true,
+    color: "#1B8A3B"
+  },
+
+  {
+    id: "aji-dulce",
+    nombre: "Ají Dulce",
+    precioKilo: 2.90,
+    disponible: true,
+    color: "#FF8C42"
+  },
+
+  {
+    id: "ajo",
+    nombre: "Ajo",
+    precioKilo: 8.00,
+    disponible: true,
+    color: "#F5F5DC"
+  },
+
+  {
+    id: "pimenton",
+    nombre: "Pimentón",
+    precioKilo: 2.70,
+    disponible: true,
+    color: "#D62828"
+  }
+
+];
